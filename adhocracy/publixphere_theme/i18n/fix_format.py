d34cf3806c5883420c18483a3dfdf65362ef9e84
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-09-02 16:12:25 +0200

    Fix fix_format
    -   display final block
    -   do not strip multiple quotes
    -   fix plural parsing

--- adhocracy/publixphere_theme/i18n/fix_format.py
+++ adhocracy/publixphere_theme/i18n/fix_format.py
@@ -10,8 +10,23 @@
 import sys
 
 
-MSGSTR = 'msgstr'
 MSGID = 'msgid'
+MSGID_PLURAL = 'msgid_plural'
+MSGSTR = 'msgstr'
+
+
+def print_block(msgid, msgid_plural, msgstr):
+    print('%s "%s"' % (MSGID, msgid))
+
+    if msgid_plural is not None:
+        print('%s "%s"' % (MSGID_PLURAL, msgid_plural))
+
+    if len(msgstr) == 1:
+        print('%s "%s"' % (MSGSTR, (msgstr[0] if msgstr[0] else msgid)))
+    else:
+        for i, s in enumerate(msgstr):
+            text = s if s else (msgid if i == 0 else msgid_plural)
+            print('%s[%i] "%s"' % (MSGSTR, i, text))
 
 
 def main():
@@ -19,13 +34,17 @@ def main():
 
     with open(fn) as fh:
         msgid = None
-        msgstr = None
+        msgid_plural = None
+        msgstr = []
 
         for line in fh:
             line = line.rstrip()
 
             if line.startswith(MSGID):
-                msgid = line[len(MSGID) + 1:].strip('"')
+                if line.startswith(MSGID_PLURAL):
+                    msgid_plural = line[len(MSGID_PLURAL) + 2:-1]
+                else:
+                    msgid = line[len(MSGID) + 2:-1]
 
             elif line.startswith(MSGSTR):
                 # do not wrap metadata at the beginning of the file
@@ -33,25 +52,33 @@ def main():
                     msgid = None
                     print('msgid ""')
                     print(line)
+                elif line.startswith(MSGSTR + '['):
+                    msgstr.append(line[len(MSGSTR) + 5:-1])
                 else:
-                    msgstr = line[len(MSGSTR) + 1:].strip('"')
+                    msgstr.append(line[len(MSGSTR) + 2:-1])
 
             elif line.startswith('"'):
                 if msgid is None:
                     print(line)
-                elif msgstr is None:
-                    msgid += line.strip('"')
+                elif msgstr == []:
+                    if msgid_plural is None:
+                        msgid += line[1:-1]
+                    else:
+                        msgid_plural += line[1:-1]
                 else:
-                    msgstr += line.strip('"')
+                    msgstr[-1] += line[1:-1]
 
             else:
                 if msgid is not None:
-                    print('msgid "%s"' % msgid)
-                    print('msgstr "%s"' % (msgstr if msgstr else msgid))
+                    print_block(msgid, msgid_plural, msgstr)
                     msgid = None
-                    msgstr = None
+                    msgid_plural = None
+                    msgstr = []
 
                 print(line)
+
+        if msgid is not None:
+            print_block(msgid, msgid_plural, msgstr)
 
 
 if __name__ == '__main__':
